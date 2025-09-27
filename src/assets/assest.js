c1fa66a6af5d3442e7ec0feba6f5f81ae5c1adbc
mayanksinghthakur <96434394+mayanksinghthakur@users.noreply.github.com>
2025-09-27 12:02:19 +0530

    Delete src directory

--- src/assets/assest.js
+++
@@ -1,9 +0,0 @@
-import logo from "./logo_home.png";
-import header from "./header.png";
-import logo_home from "./logo_home.png";
-
-export default assets = {
-  logo,
-  header,
-  logo_home,
-};
--- src/util/constants.js
+++
@@ -1,5 +0,0 @@
-const AppConstants = {
-  BACEND_URL: "http://localhost:8080/api/v1.0",
-};
-
-export default AppConstants;
